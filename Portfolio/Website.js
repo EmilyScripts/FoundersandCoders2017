34e651cc8ef22292f84836b08690433fa7033120
EmilyScripts <emily.rever@gmail.com>
2017-11-27 17:29:00 +0000

    restaurant links added, slideshow buttons placed

--- Portfolio/Website.js
+++ Portfolio/Website.js
@@ -7,7 +7,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("restaurantslides");
+    var x = document.getElementsByClassName("restaurantpic");
     if (n > x.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
@@ -15,3 +15,4 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block"; 
 }
+
